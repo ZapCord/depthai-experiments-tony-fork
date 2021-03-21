582fbb0a8b12c515e2d708f3ad7a63dbfddec6e0
ZapCord <goldcow0309@gmail.com>
2021-03-21 11:49:15 -0600

    original fork

---
+++ README.md
@@ -0,0 +1,109 @@
+# depthai-experiments
+Experimental projects we've done with DepthAI.  
+
+**Experiments** can be anything from "here's some code and it works sometimes" to "this is almost a tutorial".  
+
+The following list isn't exhaustive (as we randomly add experiments and we may forget to update this list):
+
+## [Gen2] Gaze estimation ([here](https://github.com/luxonis/depthai-experiments/tree/master/gen2-gaze-estimation))
+
+This example demonstrates how to run 3 stage (3-series, 2 parallel) inference on DepthAI using [Gen2 Pipeline Builder](https://github.com/luxonis/depthai/issues/136).
+
+[![Gaze Example Demo](https://user-images.githubusercontent.com/5244214/96713680-426c7a80-13a1-11eb-81e6-238e3decb7be.gif)](https://www.youtube.com/watch?v=OzgK5-APxBU)
+
+Origina OpenVINO demo, on which this example was made, is [here](https://github.com/LCTyrell/Gaze_pointer_controller)
+
+## [Gen2] Subpixel and LR-Check Disparity Depth ([here](https://github.com/luxonis/depthai-experiments/tree/master/gen2-camera-demo))
+
+This example shows how to do Subpixel, LR-Check or Extended Disparity, and also how to project these measurements into a point cloud for visualization.  This uses the [Gen2 Pipeline Builder](https://github.com/luxonis/depthai/issues/136).
+
+![image](https://user-images.githubusercontent.com/32992551/99454609-e59eaa00-28e3-11eb-8858-e82fd8e6eaac.png)
+![image](https://user-images.githubusercontent.com/32992551/99454680-fea75b00-28e3-11eb-80bc-2004016d75e2.png)
+
+## [Gen2] Age Gender ([here](https://github.com/luxonis/depthai-experiments/tree/master/gen2-age-gender#gen2-age--gender-recognition))
+
+This shows a simple two-stage neural inference example, doing face detection and then age/gender estimation based on the face.
+
+[![Gen2 Age & Gender recognition](https://user-images.githubusercontent.com/5244214/106005496-954a8200-60b4-11eb-923e-b84df9de9fff.gif)](https://www.youtube.com/watch?v=PwnVrPaF-vs "Age/Gender recognition on DepthAI")
+
+## [Gen2] Text Detection + Optical Character Recognition (OCR) Pipeline ([here](https://github.com/luxonis/depthai-experiments/tree/master/gen2-ocr#gen2-text-detection--optical-character-recognition-ocr-pipeline))
+
+This pipeline implements text detection (EAST) followed by optical character recognition of the detected text. 
+
+[![Text Detection + OCR on DepthAI](https://user-images.githubusercontent.com/32992551/105749667-f6315900-5f00-11eb-92bd-a297590adedc.png)](https://www.youtube.com/watch?v=YWIZYeixQjc "Gen2 OCR Pipeline")
+
+## [Gen2] Pedestrian Reidentification ([here](https://github.com/luxonis/depthai-experiments/tree/master/gen2-pedestrian-reidentification))
+
+This example demonstrates how to run 2 stage inference on DepthAI using Gen2 Pipeline Builder to identify and re-identify pedestrians with unique IDs.
+
+[![Pedestrian Re-Identification](https://user-images.githubusercontent.com/32992551/108567421-71e6b180-72c5-11eb-8af0-c6e5c3382874.png)](https://www.youtube.com/watch?v=QlXGtMWVV18 "Person Re-ID on DepthAI")
+
+Original OpenVINO demo, on which this example was made, is [here](https://docs.openvinotoolkit.org/2020.1/_demos_pedestrian_tracker_demo_README.html).
+
+## COVID-19 Mask / No-Mask Detector ([here](https://github.com/luxonis/depthai-experiments/blob/master/coronamask))
+
+This project shows you how to run the COVID-19 mask/no-mask object detector which was trained [here](https://github.com/luxonis/depthai-ml-training/tree/master/colab-notebooks#covid-19-maskno-mask-training-)
+
+[![COVID-19 mask-no-mask megaAI](https://user-images.githubusercontent.com/5244214/90733159-74436100-e2cc-11ea-8fb6-d4be937d90e5.gif)](https://photos.app.goo.gl/mJZ8TdWoNatHzW4x7 "COVID-19 mask detection")
+
+## Social Distancing Example ([here](https://github.com/luxonis/depthai-experiments/tree/master/social-distancing))
+Since DepthAI gives the full 3D position of objects in physical space, it's a couple lines of code to make a social-distancing monitor with DepthAI.  So that's what this project is, a quick cut at a social-distancing monitor.
+
+[![COVID-19 Social Distancing with DepthAI](https://user-images.githubusercontent.com/5244214/90741333-73f89500-e2cf-11ea-919b-b1f47dc55c4a.gif)](https://www.youtube.com/watch?v=-Ut9TemGZ8I "DepthAI Social Distancing Proof of Concept")
+
+## Demo-UI ([here](https://github.com/luxonis/depthai-experiments/tree/master/demo-ui))
+Application used to demonstrate various capabilities of the DepthAI platform. Contains examples with descriptions,
+console outputs and preview windows.
+
+![DemoUI](./demo-ui/preview.png)
+
+## MJPEG and JSON streaming ([here](https://github.com/luxonis/depthai-experiments/tree/master/mjpeg-streaming))
+
+[![MJPEG Streaming DepthAI](https://user-images.githubusercontent.com/5244214/90745571-92ab5b80-e2d0-11ea-9052-3f0c8afa9b0b.gif)](https://www.youtube.com/watch?v=695o0EO1Daw "DepthAI on Mac")
+
+This lay the groundwork to make DepthAI [OpenDataCam](https://github.com/opendatacam/opendatacam) compatible.
+
+## Stereo Neural Inference Results Visualizer ([here](https://github.com/luxonis/depthai-experiments/tree/master/triangulation-3D-visualizer))
+
+So because there are often application-specific host-side filtering to be done on the stereo neural inference results, and because these calculations are lightweight (i.e. could be done on an ESP32), we leave the triangulation itself to the host.  If there is interest to do this on DepthAI directly instead, please let us know!
+
+[![Spatial AI](https://user-images.githubusercontent.com/5244214/90748450-7c9e9a80-e2d2-11ea-9e9e-da65b5d9e6f0.gif)](https://www.youtube.com/watch?v=Cs8xo3mPBMg "3D Facial Landmark visualization")
+
+## People Counting ([here](https://github.com/luxonis/depthai-experiments/blob/master/people-counter))
+
+This is a basic usage example of megaAI and/or DepthAI (although it doesn't actually use the depth aspect of DepthAI): simply counting people in a scene and logging this count.
+
+So you could use this to make plots over a day of room occupancy.  One could modify this example to show *where* in a room those people were, over time, if desirable.  But for now it just produces a count of people - so the total in view of the camera - over time.
+
+[![Watch the demo](https://user-images.githubusercontent.com/5244214/90751105-fc7a3400-e2d5-11ea-82fe-3c7797e99e3e.gif)](https://youtu.be/M0xQI1kXju4)
+
+## People Tracker ([here](https://github.com/luxonis/depthai-experiments/tree/master/people-tracker))
+
+This application counts how many people went upwards / downwards / leftwards / rightwards in the video stream, allowing you to receive an information about how many people went into a room or went through a corridor.
+
+The model used in this example is [person_detection_retail_0013](https://docs.openvinotoolkit.org/latest/omz_models_intel_person_detection_retail_0013_description_person_detection_retail_0013.html) from the OpenVIN Model Zoo.  Credits: Adrian Rosebrock, OpenCV People Counter, PyImageSearch, [https://www.pyimagesearch.com/2018/08/13/opencv-people-counter/](https://www.pyimagesearch.com/2018/08/13/opencv-people-counter/), accessed on 6 August 2020.
+
+[![Watch the demo](https://user-images.githubusercontent.com/5244214/90752628-ee2d1780-e2d7-11ea-8e48-ca94b02a7674.gif)](https://youtu.be/8RiHkkGKdj0)
+
+
+## Point Cloud Projection ([here](https://github.com/luxonis/depthai-experiments/blob/master/point-cloud-projection))
+
+This is a simple application which creates rgbd image from `right` and `depth_raw` stream and projects it into point clouds. There is also a interactive point cloud visualizer. (depth_raw with left and rgb will be added soon)
+
+![point cloud visualization](https://media.giphy.com/media/W2Es1aC7N0XZIlyRmf/giphy.gif)
+
+
+## RGB-D and PCL([here](https://github.com/luxonis/depthai-experiments/tree/master/pcl-projection-rgb))
+
+This is a simple application which creates rgbd image from `rgb` and `depth` stream and projects it into rgb with depth overlay and point clouds. There is also a interactive point cloud visualizer.
+
+![rgbd](https://media.giphy.com/media/SnW9p4r3feMQGOmayy/giphy.gif)
+![rgbd-pcl](https://media.giphy.com/media/UeAlkPpeHaxItO0NJ6/giphy.gif)
+
+
+## Host-Side WLS Filter ([here](https://github.com/luxonis/depthai-experiments/tree/master/wls-filter))
+
+This gives an example of doing host-side WLS filtering using the `rectified_right` and `depth` stream from DepthAI.  
+
+Example running on [BW1092](https://shop.luxonis.com/collections/all/products/bw1092-pre-order) shown below:
+![image](https://user-images.githubusercontent.com/32992551/94463964-fc920d00-017a-11eb-9e99-8a023cdc8a72.png)
---
+++ gen2-human-pose/main.py
@@ -0,0 +1,256 @@
+import argparse
+import threading
+import time
+from pathlib import Path
+from pose import getKeypoints, getValidPairs, getPersonwiseKeypoints
+import cv2
+import depthai as dai
+import numpy as np
+from imutils.video import FPS
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-nd', '--no-debug', action="store_true", help="Prevent debug output")
+parser.add_argument('-cam', '--camera', action="store_true", help="Use DepthAI 4K RGB camera for inference (conflicts with -vid)")
+parser.add_argument('-vid', '--video', type=str, help="Path to video file to be used for inference (conflicts with -cam)")
+args = parser.parse_args()
+
+if not args.camera and not args.video:
+    raise RuntimeError("No source selected. Please use either \"-cam\" to use RGB camera as a source or \"-vid <path>\" to run on video")
+
+debug = not args.no_debug
+
+
+def cos_dist(a, b):
+    return np.dot(a, b)/(np.linalg.norm(a)*np.linalg.norm(b))
+
+
+def to_tensor_result(packet):
+    return {
+        tensor.name: np.array(packet.getLayerFp16(tensor.name)).reshape(tensor.dims)
+        for tensor in packet.getRaw().tensors
+    }
+
+def frame_norm(frame, bbox):
+    return (np.clip(np.array(bbox), 0, 1) * np.array([*frame.shape[:2], *frame.shape[:2]])[::-1]).astype(int)
+
+
+def to_planar(arr: np.ndarray, shape: tuple) -> list:
+    return cv2.resize(arr, shape).transpose(2,0,1).flatten()
+
+
+def create_pipeline():
+    print("Creating pipeline...")
+    pipeline = dai.Pipeline()
+
+    if args.camera:
+        # ColorCamera
+        print("Creating Color Camera...")
+        cam = pipeline.createColorCamera()
+        cam.setPreviewSize(456, 256)
+        cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        cam.setInterleaved(False)
+        cam.setBoardSocket(dai.CameraBoardSocket.RGB)
+        cam_xout = pipeline.createXLinkOut()
+        cam_xout.setStreamName("cam_out")
+        cam.preview.link(cam_xout.input)
+        controlIn = pipeline.createXLinkIn()
+        controlIn.setStreamName('control')
+        controlIn.out.link(cam.inputControl)
+
+    # NeuralNetwork
+    print("Creating Human Pose Estimation Neural Network...")
+    pose_nn = pipeline.createNeuralNetwork()
+    if args.camera:
+        pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_6shave.blob").resolve().absolute()))
+    else:
+        pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_8shave.blob").resolve().absolute()))
+    # Increase threads for detection
+    pose_nn.setNumInferenceThreads(2)
+    # Specify that network takes latest arriving frame in non-blocking manner
+    pose_nn.input.setQueueSize(1)
+    pose_nn.input.setBlocking(False)
+    pose_nn_xout = pipeline.createXLinkOut()
+    pose_nn_xout.setStreamName("pose_nn")
+    pose_nn.out.link(pose_nn_xout.input)
+
+    if args.camera:
+        cam.preview.link(pose_nn.input)
+    else:
+        pose_in = pipeline.createXLinkIn()
+        pose_in.setStreamName("pose_in")
+        pose_in.out.link(pose_nn.input)
+
+    print("Pipeline created.")
+    return pipeline
+
+
+colors = [[0, 100, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 0],
+          [255, 200, 100], [255, 0, 255], [0, 255, 0], [255, 200, 100], [255, 0, 255], [0, 0, 255], [255, 0, 0],
+          [200, 200, 0], [255, 0, 0], [200, 200, 0], [0, 0, 0]]
+POSE_PAIRS = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12], [12, 13],
+              [1, 0], [0, 14], [14, 16], [0, 15], [15, 17], [2, 17], [5, 16]]
+
+running = True
+pose = None
+keypoints_list = None
+detected_keypoints = None
+personwiseKeypoints = None
+
+
+class FPSHandler:
+    def __init__(self, cap=None):
+        self.timestamp = time.time()
+        self.start = time.time()
+        self.framerate = cap.get(cv2.CAP_PROP_FPS) if cap is not None else None
+
+        self.frame_cnt = 0
+        self.ticks = {}
+        self.ticks_cnt = {}
+
+    def next_iter(self):
+        if not args.camera:
+            frame_delay = 1.0 / self.framerate
+            delay = (self.timestamp + frame_delay) - time.time()
+            if delay > 0:
+                time.sleep(delay)
+        self.timestamp = time.time()
+        self.frame_cnt += 1
+
+    def tick(self, name):
+        if name in self.ticks:
+            self.ticks_cnt[name] += 1
+        else:
+            self.ticks[name] = time.time()
+            self.ticks_cnt[name] = 0
+
+    def tick_fps(self, name):
+        if name in self.ticks:
+            return self.ticks_cnt[name] / (time.time() - self.ticks[name])
+        else:
+            return 0
+
+    def fps(self):
+        return self.frame_cnt / (self.timestamp - self.start)
+
+
+if args.camera:
+    fps = FPSHandler()
+else:
+    cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
+    fps = FPSHandler(cap)
+
+
+def pose_thread(in_queue):
+    global keypoints_list, detected_keypoints, personwiseKeypoints
+
+    while running:
+        try:
+            raw_in = in_queue.get()
+        except RuntimeError:
+            return
+        fps.tick('nn')
+        heatmaps = np.array(raw_in.getLayerFp16('Mconv7_stage2_L2')).reshape((1, 19, 32, 57))
+        pafs = np.array(raw_in.getLayerFp16('Mconv7_stage2_L1')).reshape((1, 38, 32, 57))
+        heatmaps = heatmaps.astype('float32')
+        pafs = pafs.astype('float32')
+        outputs = np.concatenate((heatmaps, pafs), axis=1)
+
+        new_keypoints = []
+        new_keypoints_list = np.zeros((0, 3))
+        keypoint_id = 0
+
+        for row in range(18):
+            probMap = outputs[0, row, :, :]
+            probMap = cv2.resize(probMap, (w, h))  # (456, 256)
+            keypoints = getKeypoints(probMap, 0.3)
+            new_keypoints_list = np.vstack([new_keypoints_list, *keypoints])
+            keypoints_with_id = []
+
+            for i in range(len(keypoints)):
+                keypoints_with_id.append(keypoints[i] + (keypoint_id,))
+                keypoint_id += 1
+
+            new_keypoints.append(keypoints_with_id)
+
+        valid_pairs, invalid_pairs = getValidPairs(outputs, w, h, new_keypoints)
+        newPersonwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs, new_keypoints_list)
+
+        detected_keypoints, keypoints_list, personwiseKeypoints = (new_keypoints, new_keypoints_list, newPersonwiseKeypoints)
+
+
+with dai.Device(create_pipeline()) as device:
+    print("Starting pipeline...")
+    device.startPipeline()
+    if args.camera:
+        cam_out = device.getOutputQueue("cam_out", 1, True)
+        controlQueue = device.getInputQueue('control')
+    else:
+        pose_in = device.getInputQueue("pose_in")
+    pose_nn = device.getOutputQueue("pose_nn", 1, False)
+    t = threading.Thread(target=pose_thread, args=(pose_nn, ))
+    t.start()
+
+    def should_run():
+        return cap.isOpened() if args.video else True
+
+
+    def get_frame():
+        if args.video:
+            return cap.read()
+        else:
+            return True, np.array(cam_out.get().getData()).reshape((3, 256, 456)).transpose(1, 2, 0).astype(np.uint8)
+
+
+    try:
+        while should_run():
+            read_correctly, frame = get_frame()
+
+            if not read_correctly:
+                break
+
+            fps.next_iter()
+            h, w = frame.shape[:2]  # 256, 456
+            debug_frame = frame.copy()
+
+            if not args.camera:
+                nn_data = dai.NNData()
+                nn_data.setLayer("input", to_planar(frame, (456, 256)))
+                pose_in.send(nn_data)
+
+            if debug:
+                if keypoints_list is not None and detected_keypoints is not None and personwiseKeypoints is not None:
+                    for i in range(18):
+                        for j in range(len(detected_keypoints[i])):
+                            cv2.circle(debug_frame, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
+                    for i in range(17):
+                        for n in range(len(personwiseKeypoints)):
+                            index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+                            if -1 in index:
+                                continue
+                            B = np.int32(keypoints_list[index.astype(int), 0])
+                            A = np.int32(keypoints_list[index.astype(int), 1])
+                            cv2.line(debug_frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+                cv2.putText(debug_frame, f"RGB FPS: {round(fps.fps(), 1)}", (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.putText(debug_frame, f"NN FPS:  {round(fps.tick_fps('nn'), 1)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.imshow("rgb", debug_frame)
+
+            key = cv2.waitKey(1)
+            if key == ord('q'):
+                break
+                
+            elif key == ord('t'):
+                print("Autofocus trigger (and disable continuous)")
+                ctrl = dai.CameraControl()
+                ctrl.setAutoFocusMode(dai.CameraControl.AutoFocusMode.AUTO)
+                ctrl.setAutoFocusTrigger()
+                controlQueue.send(ctrl)            
+
+    except KeyboardInterrupt:
+        pass
+
+    running = False
+
+t.join()
+print("FPS: {:.2f}".format(fps.fps()))
+if not args.camera:
+    cap.release()
