3397bc31b8e3fb630c00e51481b38be165603ece
ZapCord <goldcow0309@gmail.com>
2021-03-21 18:25:46 -0600

    ability to use right side stereo camera for finding the keypoints

---
+++ gen2-human-pose/main2.py
@@ -0,0 +1,309 @@
+import argparse
+import threading
+import time
+from pathlib import Path
+from pose import getKeypoints, getValidPairs, getPersonwiseKeypoints
+import cv2
+import depthai as dai
+import numpy as np
+from imutils.video import FPS
+import sys
+
+
+################################################################################
+# get arguments
+################################################################################
+parser = argparse.ArgumentParser()
+parser.add_argument('-nd', '--no-debug', action="store_true", help="Prevent debug output")
+parser.add_argument('-ccam', '--ccamera', action="store_true", help="Use DepthAI 4K RGB camera for inference (conflicts with -vid and -mcam)")
+parser.add_argument('-mcam', '--mcamera', action="store_true", help="Use DepthAI stereo cameras for inference (conflicts with -vid and -ccam)")
+parser.add_argument('-vid', '--video', type=str, help="Path to video file to be used for inference (conflicts with -cam)")
+args = parser.parse_args()
+
+if not args.ccamera and not args.video and not args.mcamera:
+    raise RuntimeError("No source selected. Please use either \"-cam\" to use RGB camera as a source or \"-vid <path>\" to run on video")
+
+debug = not args.no_debug
+
+################################################################################
+# function definitions
+################################################################################
+def to_planar(arr: np.ndarray, shape: tuple) -> list:
+    return cv2.resize(arr, shape).transpose(2,0,1).flatten()
+
+def pose_thread(in_queue):
+    global keypoints_list, detected_keypoints, personwiseKeypoints
+    w=456
+    h=256
+
+    while running:
+        try:
+            raw_in = in_queue.get()
+        except RuntimeError:
+            return
+        fps.tick('nn')
+        heatmaps = np.array(raw_in.getLayerFp16('Mconv7_stage2_L2')).reshape((1, 19, 32, 57))
+        pafs = np.array(raw_in.getLayerFp16('Mconv7_stage2_L1')).reshape((1, 38, 32, 57))
+        heatmaps = heatmaps.astype('float32')
+        pafs = pafs.astype('float32')
+        outputs = np.concatenate((heatmaps, pafs), axis=1)
+
+        new_keypoints = []
+        new_keypoints_list = np.zeros((0, 3))
+        keypoint_id = 0
+
+        for row in range(18):
+            probMap = outputs[0, row, :, :]
+            probMap = cv2.resize(probMap, (w, h))  # (456, 256)
+            keypoints = getKeypoints(probMap, 0.3)
+            new_keypoints_list = np.vstack([new_keypoints_list, *keypoints])
+            keypoints_with_id = []
+
+            for i in range(len(keypoints)):
+                keypoints_with_id.append(keypoints[i] + (keypoint_id,))
+                keypoint_id += 1
+
+            new_keypoints.append(keypoints_with_id)
+
+        valid_pairs, invalid_pairs = getValidPairs(outputs, w, h, new_keypoints)
+        newPersonwiseKeypoints = getPersonwiseKeypoints(valid_pairs, invalid_pairs, new_keypoints_list)
+
+        detected_keypoints, keypoints_list, personwiseKeypoints = (new_keypoints, new_keypoints_list, newPersonwiseKeypoints)
+
+
+class FPSHandler:
+    def __init__(self, cap=None):
+        self.timestamp = time.time()
+        self.start = time.time()
+        self.framerate = cap.get(cv2.CAP_PROP_FPS) if cap is not None else None
+
+        self.frame_cnt = 0
+        self.ticks = {}
+        self.ticks_cnt = {}
+
+    def next_iter(self):
+        if args.video:
+            frame_delay = 1.0 / self.framerate
+            delay = (self.timestamp + frame_delay) - time.time()
+            if delay > 0:
+                time.sleep(delay)
+        self.timestamp = time.time()
+        self.frame_cnt += 1
+
+    def tick(self, name):
+        if name in self.ticks:
+            self.ticks_cnt[name] += 1
+        else:
+            self.ticks[name] = time.time()
+            self.ticks_cnt[name] = 0
+
+    def tick_fps(self, name):
+        if name in self.ticks:
+            return self.ticks_cnt[name] / (time.time() - self.ticks[name])
+        else:
+            return 0
+
+    def fps(self):
+        return self.frame_cnt / (self.timestamp - self.start)
+
+
+if args.ccamera or args.mcamera:
+    fps = FPSHandler()
+else:
+    cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
+    fps = FPSHandler(cap)
+
+colors = [[0, 100, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 255], [0, 100, 255], [0, 255, 0],
+          [255, 200, 100], [255, 0, 255], [0, 255, 0], [255, 200, 100], [255, 0, 255], [0, 0, 255], [255, 0, 0],
+          [200, 200, 0], [255, 0, 0], [200, 200, 0], [0, 0, 0]]
+POSE_PAIRS = [[1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12], [12, 13],
+              [1, 0], [0, 14], [14, 16], [0, 15], [15, 17], [2, 17], [5, 16]]
+
+running = True
+pose = None
+keypoints_list = None
+detected_keypoints = None
+personwiseKeypoints = None
+
+# Start defining a pipeline
+pipeline = dai.Pipeline()
+
+# Define a source - mono (grayscale) camera
+cam_right = pipeline.createMonoCamera()
+cam_right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+cam_right.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+
+# cam_left = pipeline.createMonoCamera()
+# cam_left.setBoardSocket(dai.CameraBoardSocket.LEFT)
+# cam_left.setResolution(dai.MonoCameraProperties.SensorResolution.THE_720_P)
+
+# Define a neural network that will make predictions based on the source frames
+print("Creating Human Pose Estimation Neural Network...")
+
+if args.ccamera:
+    pose_nn = pipeline.createNeuralNetwork()
+    pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_6shave.blob").resolve().absolute()))
+elif args.mcamera:
+    pose_nn_right = pipeline.createNeuralNetwork()
+    pose_nn_right.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_6shave.blob").resolve().absolute()))
+    # pose_nn_left = pipeline.createNeuralNetwork()
+    # pose_nn_left.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_6shave.blob").resolve().absolute()))
+else:
+    pose_nn.setBlobPath(str(Path("models/human-pose-estimation-0001_openvino_2021.2_8shave.blob").resolve().absolute()))
+
+# Create a node to convert the grayscale frame into the nn-acceptable form
+manip_right = pipeline.createImageManip()
+manip_right.initialConfig.setResize(456, 256)
+# The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
+manip_right.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p)
+cam_right.out.link(manip_right.inputImage)
+manip_right.out.link(pose_nn_right.input)
+
+# manip_left = pipeline.createImageManip()
+# manip_left.initialConfig.setResize(456, 256)
+# # The NN model expects BGR input. By default ImageManip output type would be same as input (gray in this case)
+# manip_left.initialConfig.setFrameType(dai.RawImgFrame.Type.BGR888p)
+# cam_left.out.link(manip_left.inputImage)
+# manip_left.out.link(pose_nn_left.input)
+
+controlIn_right = pipeline.createXLinkIn()
+controlIn_right.setStreamName('control_right')
+controlIn_right.out.link(cam_right.inputControl)
+
+# controlIn_left = pipeline.createXLinkIn()
+# controlIn_left.setStreamName('control_left')
+# controlIn_left.out.link(cam_left.inputControl)
+
+# Create outputs
+xout_manip_right = pipeline.createXLinkOut()
+xout_manip_right.setStreamName("right")
+manip_right.out.link(xout_manip_right.input)
+
+# xout_manip_left = pipeline.createXLinkOut()
+# xout_manip_left.setStreamName("left")
+# manip_left.out.link(xout_manip_left.input)
+
+# Increase threads for detection
+pose_nn_right.setNumInferenceThreads(2)
+# Specify that network takes latest arriving frame in non-blocking manner
+pose_nn_right.input.setQueueSize(1)
+pose_nn_right.input.setBlocking(False)
+xout_nn_right = pipeline.createXLinkOut()
+xout_nn_right.setStreamName("pose_nn_right")
+pose_nn_right.out.link(xout_nn_right.input)
+
+# # Increase threads for detection
+# pose_nn_left.setNumInferenceThreads(2)
+# # Specify that network takes latest arriving frame in non-blocking manner
+# pose_nn_left.input.setQueueSize(1)
+# pose_nn_left.input.setBlocking(False)
+# xout_nn_left = pipeline.createXLinkOut()
+# xout_nn_left.setStreamName("pose_nn_left")
+# pose_nn_left.out.link(xout_nn_left.input)
+
+# Pipeline defined, now the device is connected to
+with dai.Device(pipeline) as device:
+    # Start pipeline
+    device.startPipeline()
+
+    # Output queues will be used to get the grayscale frames and nn data from the outputs defined above
+    # q_left = device.getOutputQueue("left", 1, blocking=False)
+    # q_nn_left = device.getOutputQueue("pose_nn_left", 1, blocking=False)
+    q_right = device.getOutputQueue("right", 1, blocking=False)
+    q_nn_right = device.getOutputQueue("pose_nn_right", 1, blocking=False)
+    t_right = threading.Thread(target=pose_thread, args=(q_nn_right, ))
+    t_right.start()
+    # t_left = threading.Thread(target=pose_thread, args=(q_nn_left, ))
+    # t_left.start()
+
+    def should_run():
+        return cap.isOpened() if args.video else True
+
+    frame = None
+    bboxes = []
+    confidences = []
+    labels = []
+
+    try:
+        right_frame=None
+        # left_frame=None
+        while should_run():
+            fps.next_iter()
+            #h, w = frame.shape[:2]
+
+            # instead of get (blocking) used tryGet (nonblocking) which will return the available data or None otherwise
+            # in_left = q_left.tryGet()
+            # in_nn_left = q_nn_left.tryGet()
+            in_right = q_right.tryGet()
+            in_nn_right = q_nn_right.tryGet()
+
+            if in_right is not None:
+                shape = (3, in_right.getHeight(), in_right.getWidth())
+                right_frame = in_right.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+                right_frame = np.ascontiguousarray(right_frame)
+                debug_right_frame = right_frame
+
+            if in_nn_right is not None:
+                if keypoints_list is not None and detected_keypoints is not None and personwiseKeypoints is not None:
+                    for i in range(18):
+                        for j in range(len(detected_keypoints[i])):
+                            cv2.circle(debug_right_frame, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
+                    for i in range(17):
+                        for n in range(len(personwiseKeypoints)):
+                            index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+                            if -1 in index:
+                                continue
+                            B = np.int32(keypoints_list[index.astype(int), 0])
+                            A = np.int32(keypoints_list[index.astype(int), 1])
+                            cv2.line(debug_right_frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+                    # cv2.putText(debug_right_frame, f"MONO FPS: {round(fps.fps(), 1)}", (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                    # cv2.putText(debug_right_frame, f"NN FPS:  {round(fps.tick_fps('nn'), 1)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                    # cv2.imshow("mono right", debug_right_frame)
+
+            if right_frame is not None:
+                cv2.putText(right_frame, f"MONO FPS: {round(fps.fps(), 1)}", (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.putText(right_frame, f"NN FPS:  {round(fps.tick_fps('nn'), 1)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                cv2.imshow("right mono", right_frame)
+
+            # if in_left is not None:
+            #     shape = (3, in_left.getHeight(), in_left.getWidth())
+            #     left_frame = in_left.getData().reshape(shape).transpose(1, 2, 0).astype(np.uint8)
+            #     left_frame = np.ascontiguousarray(left_frame)
+            #     debug_left_frame = left_frame
+            #
+            # if in_nn_left is not None:
+            #     if keypoints_list is not None and detected_keypoints is not None and personwiseKeypoints is not None:
+            #         for i in range(18):
+            #             for j in range(len(detected_keypoints[i])):
+            #                 cv2.circle(debug_left_frame, detected_keypoints[i][j][0:2], 5, colors[i], -1, cv2.LINE_AA)
+            #         for i in range(17):
+            #             for n in range(len(personwiseKeypoints)):
+            #                 index = personwiseKeypoints[n][np.array(POSE_PAIRS[i])]
+            #                 if -1 in index:
+            #                     continue
+            #                 B = np.int32(keypoints_list[index.astype(int), 0])
+            #                 A = np.int32(keypoints_list[index.astype(int), 1])
+            #                 cv2.line(debug_left_frame, (B[0], A[0]), (B[1], A[1]), colors[i], 3, cv2.LINE_AA)
+            #         # cv2.putText(debug_left_frame, f"MONO FPS: {round(fps.fps(), 1)}", (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            #         # cv2.putText(debug_left_frame, f"NN FPS:  {round(fps.tick_fps('nn'), 1)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            #         # cv2.imshow("mono left", debug_left_frame)
+            #
+            # if left_frame is not None:
+            #     cv2.putText(left_frame, f"MONO FPS: {round(fps.fps(), 1)}", (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            #     cv2.putText(left_frame, f"NN FPS:  {round(fps.tick_fps('nn'), 1)}", (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+            #     cv2.imshow("left mono", left_frame)
+
+
+            if cv2.waitKey(1) == ord('q'):
+                break
+            elif cv2.waitKey(1) == ord('t'):
+                print("Autofocus trigger (and disable continuous)")
+                ctrl = dai.CameraControl()
+                ctrl.setAutoFocusMode(dai.CameraControl.AutoFocusMode.AUTO)
+                ctrl.setAutoFocusTrigger()
+                controlQueue.send(ctrl)
+    except KeyboardInterrupt:
+        pass
+    running = False
+t.join()
+print("FPS: {:.2f}".format(fps.fps()))
